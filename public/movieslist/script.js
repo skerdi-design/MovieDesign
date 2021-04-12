1f21109a968553dfceb419dbad20bde383618264
skerdi <skerdi@design.com>
2021-04-12 20:45:14 +0200

    some comments

--- public/movieslist/script.js
+++ public/movieslist/script.js
@@ -1,31 +1,40 @@
+const movieCatalog = document .querySelector(".movie_catalog");
+const head = document.querySelector(".head");
+
+
 const photo = document.querySelector(".mockup");
 const container = document.querySelector(".content");
-const image = document.querySelector(".img_wraper")
-const movieCatalog = document .querySelector(".movie_catalog");
-
-
+const image = document.querySelector(".img_wraper");
 document.onmousemove = mousepos;
-
 function mousepos(e) {
   let pos = {
     x: (e.pageX - window.innerWidth / 2) / 30,
     y: -(e.pageY - window.innerHeight / 2) / 15
   };
   photo.style.transform = `rotatey(${pos.x / 4}deg) rotatex(${pos.y / 3}deg)`;
-  // container.style.left = `${-(pos.x / 60)}px`;
-  // container.style.top = `${pos.y / 90}px`;
   container.style.transform = `translate(${-(pos.x / 6)}%, ${pos.y / 8}%)`;
   image.style.transform = `translate(${-(pos.x / 13)}%, ${pos.y / 17}%)`;
 }
 
 const scroll_down = document.querySelector(".scroll_butt");
+const play = document.querySelector(".play");
 scroll_down.addEventListener("click",()=>{
-  movieCatalog.style.transform = `translateY(-${window.innerHeight}px)`
+  movieCatalog.style.transform = `translateY(-${window.innerHeight}px)`;
+  head.classList.add("unready");
+  head.classList.remove("ready");
 })
-
+play.addEventListener("click",()=>{
+  movieCatalog.style.transform = `translateY(-${window.innerHeight}px)`;
+  head.classList.add("unready");
+  head.classList.remove("ready");
+})
 const scroll_up = document.querySelector(".scroll_up");
 scroll_up.addEventListener("click",()=>{
-  movieCatalog.style.transform = `translateY(0px)`
+  movieCatalog.style.transform = `translateY(0px)`;
+  head.classList.remove("unready");
+  setTimeout(() => {
+    head.classList.add("ready");
+  }, 2500);
 })
 
 
@@ -51,11 +60,16 @@ function scroll(){
     trees6.style.transform = "translateY(" + x / 90 + "%)";
     svg_container.style.transform = "translateY("+ x / 3 +"%)";
     cloud.style.transform = "translateY("+x/4.8+"%)";
-    // console.count("hi")
   }
   requestAnimationFrame(scroll);
-  // const regex = /\d/g;
-  // console.log(x.match(regex).join(""));
-  // console.log(x.style);
-  // console.count("hi")
+}
+
+function classReady(){
+    setTimeout(() => {
+      head.classList.add("ready");
+    }, 2500);
+  svg_container.classList.add("ready");
+}
+window.onload = ()=>{
+  classReady();
 }
