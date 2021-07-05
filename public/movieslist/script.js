7fa4e8013b450186a19d8223e1277034bb62cefe
skerdi <skerdi@design.com>
2021-07-05 22:45:49 +0200

    design changes

--- public/movieslist/script.js
+++ public/movieslist/script.js
@@ -1,244 +1,215 @@
-let web_parent = document.querySelector(".web_parent")
-let images = document.querySelector(".images");
-let trees1 = document.querySelector(".parallax_1")
-let trees2 = document.querySelector(".parallax_2");
-let trees3 = document.querySelector(".parallax_3");
-let trees4 = document.querySelector(".parallax_4");
-let trees5 = document.querySelector(".parallax_5");
-let mockupContainer = document.querySelector(".mockup_container");
-let headerContainer = document.querySelector(".header_container");
+let webParent,sectionLines;
+//left nav mapping ===========================================================================================
+let mappedH,mappedS,mappedL,headScale,bodyScale;
+//parallax background images =================================================================================
+const para1 = document.querySelector(".parallax_1");
+const para2 = document.querySelector(".parallax_2");
+const para3 = document.querySelector(".parallax_3");
+const para4 = document.querySelector(".parallax_4");
+const para5 = document.querySelector(".parallax_5");
+const text = document.querySelector(".text_wraper");
+//right body nav =============================================================================================
+const rightNav = document.querySelector(".right_nav");
+const options = document.querySelectorAll(".options");
+//search wraper
+const searchButt = document.querySelector(".search_butt");
+const searchinput = document.querySelector(".search_input");
+//ouside left nav
+const leftNav = document.querySelector(".outside_left_nav");
+const topNav = document.querySelector(".outside_top_nav");
 
-let rightLine = document.querySelector(".right_line");
+
+function lerp (start, end, amt){
+  return (1-amt)*start+amt*end
+}
+function constrain (n, low, high) {
+  return Math.max(Math.min(n, high), low);
+};
+function mapping (target,x1,x2,y1,y2,withinBounds){
+  let newVal = ((target-x1)/(x2-x1))*(y2-y1)+y1;
+  if(!withinBounds){
+    return newVal
+  }
+  if (y1 < y2) {
+    return constrain(newVal, y1, y2);
+  } else {
+    return constrain(newVal, y2, y1);
+  }
+}
+
+
+function randomNumber(min, max) {
+  const r = Math.random() * (max - min) + min;
+  return r;
+}
 
 
 
+//------------------------------------------------------------------------------------------------------------
+function handleScrolling () {
+  webParent = document.querySelector(".web_parent");
+  let Scrollbar = window.Scrollbar;
+  let opt = {
+    damping: 0.09,
+    renderByPixels: false
+  };
+
+  class EdgeEasingPlugin extends Scrollbar.ScrollbarPlugin {
+    static pluginName = "edgeEasing";
+    _remainMomentum = {
+      x: 0,
+      y: 0
+    };
+    transformDelta(delta) {
+      const { limit, offset } = this.scrollbar;
+      const x = this._remainMomentum.x + delta.x;
+      const y = this._remainMomentum.y + delta.y;
+      // clamps momentum within [-offset, limit - offset]
+      this.scrollbar.setMomentum(
+        Math.max(-offset.x, Math.min(x, limit.x - offset.x)),
+        Math.max(-offset.y, Math.min(y, limit.y - offset.y))
+      );
+      return { x: 0, y: 0 };
+    }
+    onRender(remainMomentum) {
+      Object.assign(this._remainMomentum, remainMomentum);
+    }
+  }
+  Scrollbar.use(EdgeEasingPlugin);
+  const scrollMethods = Scrollbar.init(webParent,opt);
+
+  //scroll event funcrtions ================================================================================
+  sectionLines = document.querySelector(".section_lines");
+  const headLine = sectionLines.querySelector(".head_line");
+  const bodyLine = sectionLines.querySelector(".body_line");
+  const root = document.documentElement;
+
+  scrollMethods.addListener((info)=>{
+    let offset = info.offset.y;
+    leftNavMapping(offset,root);
+
+    teggleScrollText(offset);
+
+    leftNavUpdate(offset)
+  });
+};
+//------------------------------------------------------------------------------------------------------------
+searchButt.addEventListener("click",()=>{
+  optionsToggle(searchButt,'active');
+  optionsToggle(searchinput,'active');
+})
 
 
 
+function leftNavUpdate(x) {
+  if(x > window.innerHeight-100){
+    leftNav.classList.add("down");
+    topNav.classList.add("down");
+  }else{
+    leftNav.classList.remove("down");
+    topNav.classList.remove("down");
+  };
+};
 
-const options = document.querySelectorAll("#options");
+function optionsToggle (element,className) {
+  element.classList.toggle(className);
+};
+
+function teggleScrollText(x) {
+  let scroll = document.querySelector(".scroll_text");
+  if(x>40){
+    scroll.classList.add("down");
+  }else{
+    scroll.classList.remove("down");
+  };
+};
+
+window.onload = () =>{
+  handleScrolling();
+  const scrollContent = document.querySelector(".scroll-content");
+  requestAnimationFrame(animate)
+  function animate() {
+    let topOffset = scrollContent.getBoundingClientRect().top
+    //animation that run only on the head
+    if(-(topOffset) < window.innerHeight +50){
+      parallaxAnimation(-(topOffset))
+      canvasAnimation ()
+    }
+    //animation that run only on the body
+    if(-(topOffset) > window.innerHeight + 50){
+      rightNav.style.transform = `translateY(${-(topOffset)-window.innerHeight - 50}px)`;
+    }else{
+      rightNav.style.transform = `translateY(0px)`;
+    };
+    requestAnimationFrame(animate);
+  };
+};
+
+
+
 options.forEach(x=>x.addEventListener("click",()=>{
   options.forEach(y=>{
-    y.classList.remove("toggled");
+    y.classList.remove("active");
   })
-  x.classList.add("toggled");
+  x.classList.add("active");
+  // console.log(x.classList[1])
+  // if(x.classList[1] === "edit"){
+  //   moviesList.classList.add("edit_mode")
+  // }else{
+  //   moviesList.classList.remove("edit_mode")
+  // }
 }))
 
 
 
-
-const scrolltxt = document.querySelector("#scroll");
-const section = document.querySelector("#section");
-const sectionBody = document.querySelector("#section_body");
-
-
-
-const navBar = document.querySelector(".nav_bar_wraper");
-const verLinks = document.querySelector(".vertical_links");
-
-// const title = document.querySelector(".title");
-
-
-let scrollContent;
-let scrolling = 0;
-
-let Scrollbar = window.Scrollbar;
-let opt = {
-  damping: 0.095,
-}
-const scrollmethods = Scrollbar.init(web_parent,opt);
-scrollmethods.addListener((status)=>{
-
-
-  if(status.offset.y > 20){
-    scrolltxt.classList.add("down");
-  }else{
-    scrolltxt.classList.remove("down");
-  }
-  if(status.offset.y > window.innerHeight/1.1){
-    navBar.classList.add("down");
-    verLinks.classList.add("down");
-  }else{
-    navBar.classList.remove("down");
-    verLinks.classList.remove("down");
-  }
-  if(status.offset.y > window.innerHeight/ 1.5){
-    section.classList.add("down");
-    sectionBody.classList.add("down");
-  }else{
-    section.classList.remove("down");
-    sectionBody.classList.remove("down");
-    // title.classList.remove("down");
-  }
-
-  // let st = status.offset.y;
-  // if(st > scrolling){
-  //   console.log("scrolling down");
-  //   // topNav.style.opacity = '0.1';
-  //   for(let i =0;i<numParticels;i++){
-  //     particle.speed = particle.speed + 100;
-  //   }
-  // }else if(st < scrolling){
-  //   console.log("scrolling up");
-  //   // topNav.style.opacity = '1';
-  // }
-  // scrolling = st <= 0 ? 0 : st;//scrolling = if st is smaller or equal than 0 is 0 otherwise it is = to st
-})
-// console.log({Scrollbar});
-// console.log(scrollmethods.offset.y);
-
-
-
-
-
-
-
-let rightNav = document.querySelector(".right_nav");
-// let images = document.querySelector(".images");
-
-
-// let topNav = document.querySelector(".top_nav");
-  scrollContent = document.querySelector(".scroll-content");
-  requestAnimationFrame(scroll);
-  function scroll(){
-    let x = -(scrollContent.getBoundingClientRect().top);
-  //x is the amount of px the window has moved verticly
-  //the translateY
-  if(x < window.innerHeight +30){
-    trees1.style.transform = "translateY(" + x / 5.5 + "%)";//mountain
-    trees2.style.transform = "translateY(" + x / 7 + "%)";//darker mountains
-    trees3.style.transform = "translateY(" + x / 9 + "%)";//first trees      //the final one
-    trees4.style.transform = "translateY(" + x / 12 + "%)";
-    trees5.style.transform = "translateY(" + x / 17 + "%)";
-    mockupContainer.style.transform = "translateY(" + x / 10 + "%)";
-    headerContainer.style.transform = "translateY(" + x / 10 + "%)";
-  }
-  // if(x > window.innerHeight/1.5){
-  //   topNav.style.opacity = 1;
-  // }else{
-  //   topNav.style.opacity = 0;
-  // }
-  if(x>window.innerHeight + 30){
-    //for the body top_nav
-    // console.log("hi")
-    rightNav.style.transform = `translateY(${x-window.innerHeight - 30}px)`;
-    // canvas.style.transform = `translateY(${x-window.innerHeight + 30}px)`;
-    // topNav.style.transform = `translateY(${x-window.innerHeight -30}px)`;
-    //scrolling handler ==============================================================================
-    //let st = x;
-    // if(st > scrolling){
-      // console.log("scrolling down");
-      // topNav.style.opacity = '0.1';
-    // }else if(st < scrolling){
-      // console.log("scrolling up");
-      // topNav.style.opacity = '1';
-    // }
-    //scrolling = st <= 0 ? 0 : st;//scrolling = if st is smaller or equal than 0 is 0 otherwise it is = to st
-    // pop.style.top = 40 + body.scrollTop+"px";
-  }else{
-    rightNav.style.transform = `translateY(0px)`;
-  }
-  requestAnimationFrame(scroll);
-}
-
-
-
-
-
-
-const photo = document.querySelector(".mockup");
-const container = document.querySelector(".content");
-const image = document.querySelector(".img_wraper");
-document.onmousemove = mousepos;
-function mousepos(e) {
-  let pos = {
-    x: (e.pageX - window.innerWidth / 2) / 30,
-    y: -(e.pageY - window.innerHeight / 2) / 15
-  };
-  photo.style.transform = `rotatey(${pos.x / 4}deg) rotatex(${pos.y / 3}deg)`;
-  container.style.transform = `translate(${-(pos.x / 8)}%, ${pos.y / 6}%)`;
-  image.style.transform = `translate(${-(pos.x / 13)}%, ${pos.y / 6}%)`;
-}
-
-// let search_icon = document.querySelector(".search_icon");
-// let search = document.querySelector(".search");
-// search_icon.addEventListener("click",()=>{
-//   let bool = false
-//   for(let i = 0; i < search.classList.length;i++){
-//     if(search.classList[i]=="open"){
-//       bool = true
-//     }
-//   }
-//   if(bool == false){
-//     search.classList.add("open");
-//   }else{
-//     search.classList.remove("open");
-//   }
-// })
-
-
-let searchButt = document.querySelector(".search_butt");
-let searchInput = document.querySelector(".search_input");
-searchButt.addEventListener("click",()=>{
-  searchButt.classList.toggle("focus");
-  searchInput.classList.toggle("searching");
-});
-
-
-
-// let state = true;
-
-// sliderEle.addEventListener("click",()=>{
-//   if(state == true){
-//     sliderEle.classList.add("focus");
-//   }else{
-//     sliderEle.classList.remove("focus");
-//   }
-//   state = true;
-//   console.log("added");
-// })
-
 const ele = document.querySelectorAll(".ele");
-ele.forEach(x=>{x.addEventListener("click",()=>{
-  x.classList.toggle("toggled")
+ele.forEach(x=>{x.addEventListener("click",(e)=>{
+  // }else{
+  //   x.classList.add("toggled")
+  // }
+  // if(e.target == x.querySelector(".close_wraper")){
+  //   x.classList.remove("toggled")
+  // }
+  if(e.target == x.querySelector(".bookmark_wraper")){
+    x.parentElement.classList.toggle("fav");
+  }else{
+    x.parentElement.classList.toggle("toggled")
+  }
 })})
 
 
 
 
+function parallaxAnimation(x) {
+  text.style.transform = "translateY(" + x / 8.5 + "%)";
+  para1.style.transform = "translateY(" + x / 5.5 + "%)";
+  para2.style.transform = "translateY(" + x / 7 + "%)";
+  para3.style.transform = "translateY(" + x / 9 + "%)";
+  para4.style.transform = "translateY(" + x / 12 + "%)";
+  para5.style.transform = "translateY(" + x / 17 + "%)";
+}
+
+
+function leftNavMapping(x,ele){
+  mappedH = Math.floor(mapping(x,0,window.innerHeight,305,338,true));
+  mappedS = Math.floor(mapping(x,0,window.innerHeight,72,100,true));
+  mappedL = Math.floor(mapping(x,0,window.innerHeight,9,45,true));
+  headScale = Math.floor(mapping(x,0,window.innerHeight+200 ,100,40,true));
+  bodyScale = Math.floor(mapping(x,0,window.innerHeight+200 ,40,100,true));
+
+  ele.style.setProperty("--line-color",`hsl(${mappedH}, ${mappedS}%, ${mappedL}%)`)
+  ele.style.setProperty("--head-scale",`scaleX(${headScale}%)`)
+  ele.style.setProperty("--body-scale",`scaleX(${bodyScale}%)`)
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//canvas animation ====================================================================================
 const canvas = document.querySelector(".canvas");
 let ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
+canvas.width = window.innerWidth*1.1;
 canvas.height = window.innerHeight/2.8;
+
 if(window.innerWidth < 600){
   canvas.height = window.innerHeight/3.5;
 }
@@ -247,7 +218,8 @@ if(window.innerWidth < 600){
 let particle;
 let array = []
 let time = 0;
-let numParticels = Math.floor(window.innerWidth / 8);
+let numParticels;
+
 
 class Particles {
   constructor(x,y,r){
@@ -258,8 +230,8 @@ class Particles {
       r:this.x+randomNumber(40,100),
       l:this.x-randomNumber(40,100)
     }
-    this.speed = -(this.r+2)/6;
-    this.bounce = randomNumber(this.r/2,-this.r/2);
+    this.speed = -this.r/14;
+    this.bounce = randomNumber(-this.speed,this.speed);
   }
   update(){
     this.y = this.y + this.speed;
@@ -267,12 +239,12 @@ class Particles {
       this.y = canvas.height+this.r/2
     }
     if(this.x+this.r/2>this.tube.r){
-      this.bounce = -1
+      this.bounce = -0.2
     }else if(this.x-this.r/2<this.tube.l){
-      this.bounce = 1
+      this.bounce = 0.2
     }
     this.x = this.x + this.bounce;
-    this.alpha = maping(this.y,canvas.height,0-this.r/2,1,0);
+    this.alpha = mapping(this.y,canvas.height,0-this.r/2,1,0.2);
   }
   paint(){
     ctx.beginPath();
@@ -282,51 +254,32 @@ class Particles {
     ctx.fill();
   }
 }
-
-
-for(let i = 0;i < numParticels;i++){
-  particle = new Particles(randomNumber(0,canvas.width),randomNumber(0,canvas.height),randomNumber(1,4));
-  array.push(particle)
+windowResize();
+function windowResize(){
+  window.addEventListener('resize', ()=>{
+    canvas.width = window.innerWidth + 90
+    redraw()
+  });
+  redraw();
 }
-setInterval(()=>{
+function redraw() {
+  array = [];
+  numParticels = Math.floor(window.innerWidth / 10)
+  for(let i = 0;i < numParticels;i++){
+    if(canvas.width <= 900){
+      particle = new Particles(randomNumber(0,canvas.width),randomNumber(0,canvas.height),randomNumber(1,3));
+    array.push(particle)
+    }else{
+      particle = new Particles(randomNumber(0,canvas.width),randomNumber(0,canvas.height),randomNumber(1,3.5));
+    array.push(particle)
+    }
+  }
+}
+
+function canvasAnimation () {
   ctx.clearRect(0,0,canvas.width,canvas.height);
   for(let i = 0;i < numParticels;i++){
     array[i].update();
     array[i].paint();
   }
-}, 60)
-
-
-window.addEventListener('resize',()=>{
-  time = 2;
-  canvas.width = window.innerWidth;
-  if(window.innerWidth < 600){
-    canvas.height = window.innerHeight/3.5;
-  }else{
-    canvas.height = window.innerHeight/2.8;
-  }
-  numParticels = Math.floor(canvas.width / 8);
-  let interval = setInterval(()=>{
-    time = time - 1;
-    if(time <= 0){
-      array = [];
-      for(let i =0;i<numParticels;i++){
-        particle = new Particles(randomNumber(0,canvas.width),randomNumber(0,canvas.height),randomNumber(1,4));
-        array.push(particle);
-      }
-      clearInterval(interval);
-    }
-    
-    
-  },300)
-});
-function randomNumber(min, max) {
-  const r = Math.random() * (max - min) + min;
-  return r;
-}
-function maping(init, a, b, c, d) {
-  // first map value from (a..b) to (0..1)
-  let value = (init - a) / (b - a);
-  // then map it from (0..1) to (c..d) and return it
-  return c + value * (d - c);
 }
